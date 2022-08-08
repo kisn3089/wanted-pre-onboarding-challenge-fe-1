a0a54f1cde9bc3c49af4369db64c366a2c5c3d2b
insung <96061695+kisn3089@users.noreply.github.com>
2022-08-09 00:13:18 +0900

    form input work

--- client/src/components/auth/Auth.jsx
+++ client/src/components/auth/Auth.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Auth.css";
 
 const Auth = () => {
-  return <div>Login</div>;
+  return (
+    <section>
+      <form className="form">
+        <div className="text">Login</div>
+        <div className="wrap">
+          <div className="form-row">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="email"
+              id="email"
+              placeholder="test@test.com"
+            />
+          </div>
+          <div className="form-row">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="password" id="password" />
+          </div>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Auth;
